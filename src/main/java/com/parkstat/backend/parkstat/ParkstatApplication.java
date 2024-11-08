841e00d58866cea98c23b53c8eac7b3701784216
da1zyer <da1zyer@gmail.com>
2024-11-08 22:38:01 +0300

    Initial commit

---
+++ src/main/java/com/parkstat/backend/parkstat/ParkstatApplication.java
@@ -0,0 +1,13 @@
+package com.parkstat.backend.parkstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkstatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkstat/backend/parkstat/ParkstatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkstat.backend.parkstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkstatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
