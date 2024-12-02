8b4055d060f47fcdf89a65dc4e33ebc47c4c2f5c
da1zyer <da1zyer@gmail.com>
2024-12-02 14:32:02 +0300

    StatController refactoring

--- src/main/java/com/parkstat/backend/parkstat/controllers/StatController.java
+++ src/main/java/com/parkstat/backend/parkstat/controllers/StatController.java
@@ -3,10 +3,8 @@ package com.parkstat.backend.parkstat.controllers;
 import com.parkstat.backend.parkstat.models.Parking;
 import com.parkstat.backend.parkstat.models.log.CarEvent;
 import com.parkstat.backend.parkstat.models.log.Log;
-import com.parkstat.backend.parkstat.models.user.User;
 import com.parkstat.backend.parkstat.repositories.LogRepository;
-import com.parkstat.backend.parkstat.repositories.ParkingRepository;
-import com.parkstat.backend.parkstat.service.UserService;
+import com.parkstat.backend.parkstat.service.ParkingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,12 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Tag(name = "Statistics")
 @RestController
@@ -28,11 +23,9 @@ import java.util.Optional;
 @SecurityRequirement(name = "bearerAuth")
 public class StatController {
     @Autowired
-    private UserService userService;
-    @Autowired
-    private ParkingRepository parkingRepository;
-    @Autowired
     private LogRepository logRepository;
+    @Autowired
+    private ParkingService parkingService;
 
     @Operation(summary = "Get logs")
     @ApiResponses(value = {
@@ -67,30 +60,18 @@ public class StatController {
             @RequestParam(required = true) int parkingId,
             @RequestParam(required = false) CarEvent event,
             @RequestParam(required = false) String plate) {
-        User user = userService.getUserFromHeader(authHeader);
-        Optional<Parking> parkingOptional = parkingRepository.findById(parkingId);
-        if (parkingOptional.isPresent()) {
-            Parking parking = parkingOptional.get();
-            if (parking.getUser().getId() == user.getId()) {
-                if (event != null) {
-                    if (plate != null) {
-                        return logRepository.findByParkingIdAndEventAndCar_Plate(parkingId, event, plate);
-                    }
-                    return logRepository.findByParkingIdAndEvent(parkingId, event);
-                }
-                else {
-                    if (plate != null) {
-                        return logRepository.findByParkingIdAndCar_Plate(parkingId, plate);
-                    }
-                    return logRepository.findByParkingId(parkingId);
-                }
+        Parking parking = parkingService.getParking(parkingId, authHeader);
+        if (event != null) {
+            if (plate != null) {
+                return logRepository.findByParkingIdAndEventAndCar_Plate(parkingId, event, plate);
             }
-            else {
-                throw new ResponseStatusException(HttpStatus.CONFLICT, "Parking does not belong to the user");
-            }
+            return logRepository.findByParkingIdAndEvent(parkingId, event);
         }
         else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Parking not found");
+            if (plate != null) {
+                return logRepository.findByParkingIdAndCar_Plate(parkingId, plate);
+            }
+            return logRepository.findByParkingId(parkingId);
         }
     }
 }
